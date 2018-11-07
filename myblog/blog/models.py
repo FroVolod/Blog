9eab0f4d5ef8a1e1b89a0fc5ee1bb8cb28b22110
Volodymyr Frolov <volodymyr@yaelconsulting.com>
2018-11-07 21:05:23 +0200

    Create model Post

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from time import time
+
+
+def gen_slug(s):
+    return slugify(s) + '-' + str(int(time()))
+
+class Post(models.Model):
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
+    body = models.TextField(blank=True, db_index=True)
+    create_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
