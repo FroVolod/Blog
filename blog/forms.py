f9a3328c7fb968b206c754a4747fcdb070ab98a4
Volodymyr Frolov <volodymyr@yaelconsulting.com>
2018-11-09 17:19:27 +0200

    created view for tags

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Tag
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'body']
+        fields = ['title', 'body', 'tags']
+
+
+class TagForm(forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = ['title']
--- blog/views.py
+++ blog/views.py
@@ -3,28 +3,35 @@ from django.views.generic import View
 from django.urls import reverse
 
 
-from .forms import PostForm
-from .models import Post
+from .forms import PostForm, TagForm
+from .models import Post, Tag
+from .utils import ObjectCreateMixin, ObjectDetailMixin
 
 
 def posts_list(request):
     return render(request, 'blog/posts_list.html')
 
 
-class PostCreate(View):
-    def get(self, request):
-        form = PostForm()
-        return render(request, 'blog/post_create.html', context={'form': form})
-
-    def post(self, request):
-        bound_form = PostForm(request.POST)
-        if bound_form.is_valid():
-            new_object = bound_form.save()
-            return redirect(new_object)
-        return render(request, 'blog/post_create.html', context={'form': bound_form})
+def tags_list(request):
+    return render(request, 'blog/tags_list.html')
 
 
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create.html'
+
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
