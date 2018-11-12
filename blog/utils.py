328a9a83f11f8352e4fcd3d51f5cec382a44e245
Volodymyr Frolov <volodymyr@yaelconsulting.com>
2018-11-12 13:57:04 +0200

    Added views for "Update" and "Delete" objects

--- blog/utils.py
+++ blog/utils.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 class ObjectCreateMixin:
@@ -24,3 +25,37 @@ class ObjectDetailMixin:
     def get(self, request, slug):
         obj = self.model.objects.get(slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+
+class ObjectUpdateMixin:
+    model = None
+    model_form = None
+    template = None
+    
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form()
+        return render(request, self.template, context={'form': bound_form, self.model.__name__.lower(): obj})
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(request.POST, instance=obj)
+        if bound_form.is_valid():
+            update_obj = bound_form.save()
+            return redirect(update_obj)
+        return render(request, self.template, context={'form': bound_form, self.model.__name__.lower(): obj})
+
+
+class ObjectDeleteMixin:
+    model = None
+    template = None
+    redirect_template = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        obj.delete()
+        return redirect(reverse(self.redirect_template))
